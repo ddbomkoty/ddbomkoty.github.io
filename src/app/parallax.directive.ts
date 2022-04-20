5f67330a86d4757e5b7aecb506292d8d3b4116ae
mkoty <mkoty@yandex.ru>
2022-04-20 03:49:38 +0300

    added team block;

--- src/app/parallax.directive.ts
+++ src/app/parallax.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import { ChangeDetectorRef, Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 import { fromEvent } from "rxjs";
 import { debounceTime, distinctUntilChanged } from "rxjs/operators";
 
@@ -8,7 +8,11 @@ import { debounceTime, distinctUntilChanged } from "rxjs/operators";
 })
 export class ParallaxDirective {
 
+  DEBOUNCE_TIME_MS = 20;
+  previousTime: number;
+
   @Input('appParallax') factor: number;
+  @Input('screenBefore') screenBefore?: number = 0;
 
   constructor(private el: ElementRef,
               private renderer: Renderer2) {
@@ -20,12 +24,29 @@ export class ParallaxDirective {
   }
 
   onListenerTriggered(event: Event): void {
+    const currentTime = Date.now();
+    if (this.previousTime && (currentTime - this.previousTime) < this.DEBOUNCE_TIME_MS) {
+      return;
+    }
 
-    const rect = this.el.nativeElement.getBoundingClientRect();
-    const topShown = rect.top >= 0;
-    const bottomShown = rect.bottom <= window.innerHeight;
+    const element = this.el.nativeElement;
+    const rect = element.getBoundingClientRect();
+    const topShown = rect.top >= 0 && rect.top <= window.innerHeight;
+    const bottomShown = rect.bottom <= window.innerHeight && rect.bottom >= 0;
     const isTestDivScrolledIntoView = topShown || bottomShown;
 
-    isTestDivScrolledIntoView && this.renderer.setStyle(this.el.nativeElement, 'top', `${window.scrollY / this.factor}px`);
+    const spaceFromElementTopToWindowTop = rect.top;
+    // const spaceFromElementBottomToWindowBottom = window.innerHeight - (element.offsetTop + element.offsetHeight);
+    const spaceFromElementBottomToWindowBottom = window.innerHeight - (rect.top + rect.height);
+
+    // const isPlus = spaceFromElementTopToWindowTop > spaceFromElementBottomToWindowBottom;
+
+    // const parallaxDelta = (isPlus ? 1 : -1) * (window.innerHeight / 2 - rect.height / 2 - (isPlus ? rect.top : spaceFromElementBottomToWindowBottom));
+
+
+
+    isTestDivScrolledIntoView && this.renderer.setStyle(this.el.nativeElement, 'top', `${(window.scrollY - (this.screenBefore || 0) * window.innerHeight) / this.factor}px`);
+    // isTestDivScrolledIntoView && this.renderer.setStyle(this.el.nativeElement, 'top', `${(spaceFromElementTopToWindowTop - spaceFromElementBottomToWindowBottom) / this.factor}px`);
+    this.previousTime = currentTime;
   }
 }
